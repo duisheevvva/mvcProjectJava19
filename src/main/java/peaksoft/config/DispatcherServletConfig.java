64342a775c8b2cd6fbe7414eba3df6e6d38a07b2
duisheevvva <duisheeva.aizat@icloud.com>
2025-11-12 17:19:37 +0600

    firstcommit

---
+++ src/main/java/peaksoft/config/DispatcherServletConfig.java
@@ -0,0 +1,22 @@
+package peaksoft.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
